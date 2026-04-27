4b2a4f78002839f5a37ca426496ef66e0d9b6dfa
atharv-ai <en23cs301220@medicaps.ac.in>
2026-04-27 19:52:42 +0530

    express connect and mongobd also

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI;
+
+app.use(express.json());
+
+app.get("/",function(req,res){
+    res.send("hi there");
+})
+
+mongoose.connect(MONGO_URI)
+.then(()=>{
+    console.log("server connected");
+    app.listen(PORT,()=>{
+        console.log("DB Cinnected Successfully");
+    });
+})
+.catch((err)=>{
+    console.log(err);
+})
+
